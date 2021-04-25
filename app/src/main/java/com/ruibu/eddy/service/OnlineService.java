d2eb05c0df9bab6211f63d3d3a24952847d1ee24
l-deie <xuningandhua717@163.com>
2021-04-25 12:10:07 +0800

    CRTcpClient

--- app/src/main/java/com/ruibu/eddy/service/OnlineService.java
+++ app/src/main/java/com/ruibu/eddy/service/OnlineService.java
@@ -1,7 +1,41 @@
 package com.ruibu.eddy.service;
 
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import org.ddpush.im.v1.client.appuser.Message;
+import org.ddpush.im.v1.client.appuser.TCPClientBase;
+
 /**
  * Created by Eddy on 2015/3/1.
  */
-public class OnlineService {
+public class OnlineService  extends Service {
+
+    public class  CRTcpClient extends TCPClientBase{
+
+        public CRTcpClient(byte[] uuid, int appid, String serverAddr, int serverPort, int connectTimeout) throws Exception {
+            super(uuid, appid, serverAddr, serverPort, connectTimeout);
+        }
+
+        @Override
+        public boolean hasNetworkConnection() {
+            return false;
+        }
+
+        @Override
+        public void trySystemSleep() {
+
+        }
+
+        @Override
+        public void onPushMessage(Message message) {
+
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
